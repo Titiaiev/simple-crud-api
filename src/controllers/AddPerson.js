70aa23d517c90c11f0875252b98b82c5eac7caca
Maksim Titiaiev <gipsmas@gmail.com>
2021-11-30 00:12:13 +0200

    refactor: add constants responses

--- src/controllers/AddPerson.js
+++ src/controllers/AddPerson.js
@@ -1,5 +1,6 @@
 const { Person } = require('../models/person.js')
 const { Controller } = require('../lib/controller/index.js')
+const { invalidParamsRespons, createdRespons } = require('../lib/constants.js')
 
 class AddPerson extends Controller {
   static route () {
@@ -9,20 +10,14 @@ class AddPerson extends Controller {
     }
   }
 
-  controller (body, params) {
+  controller (body, _) {
     try {
       const createdUser = new Person().add(body)
       if (createdUser) {
-        return {
-          code: 201,
-          res: createdUser
-        }
+        return createdRespons(createdUser)
       }
     } catch (e) {
-      return {
-        code: 400,
-        res: e.message
-      }
+      return invalidParamsRespons(e.message)
     }
   }
 }
